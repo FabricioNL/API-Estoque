d7b3c73ac23c9cd93ef0df090cee928d8146418d
Fabricio2530 <fabricio.neri.ismart@gmail.com>
2022-10-29 12:00:48 -0300

    subindo arquivos fase 2

---
+++ crud.py
@@ -0,0 +1,61 @@
+from sqlalchemy.orm import Session
+
+import models, schemas
+
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+
+def get_user_by_email(db: Session, email: str):
+    return db.query(models.User).filter(models.User.email == email).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    fake_hashed_password = user.password
+    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def delete_user(db: Session, user_id: int):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db.delete(db_user)
+    db.commit()
+    return {"info": "usuario deletado com sucesso"}
+
+def update_user(db: Session, user_id: int, user: schemas.UserCreate):
+    db_user = db.query(models.User).filter(models.User.id == user_id).first()
+    db_user.email = user.email
+    db_user.hashed_password = user.password
+    
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_items(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Item).offset(skip).limit(limit).all()
+
+def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
+    db_item = models.Item(**item.dict(), owner_id=user_id)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def delete_user_item(db: Session, item_id: int):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    db.delete(db_item)
+    db.commit()
+    return {"info": "item deletado com sucesso"}
+
+def update_user_item(db: Session, item_id: int, user_id: int, item: schemas.ItemCreate):
+    db_item = db.query(models.Item).filter(models.Item.id == item_id and models.Item.owner_id == user_id).first()
+    db_item.title = item.title
+    db_item.description = item.description
+    
+    db.commit()
+    db.refresh(db_item)
+    return db_item
--- main.py
+++ main.py
@@ -1,210 +1,79 @@
-from operator import gt
-from fastapi import FastAPI, Path, Query, HTTPException, Body, status
-from pydantic import BaseModel
-from fastapi.exceptions import RequestValidationError
-from fastapi.exceptions import RequestValidationError
-from fastapi.exception_handlers import http_exception_handler, request_validation_exception_handler
-from starlette.exceptions import HTTPException as StarletteHTTPException
-from enum import Enum
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
 
+import crud, models, schemas
+from database import SessionLocal, engine
 
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-#exemplo de base de dados
-items = [
-    {"nome": "Foo", "preco": 50.2},
-    {"nome": "Bar", "descricao": "The bartenders", "preco": 62},
-    {"nome": "Baz", "descricao": None, "preco": 50.2,  "tags": []}
-]
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-#classe para ITEM
-class Item(BaseModel):
-    nome: str
-    descricao: str | None = None
-    preco: float 
-    tags: list[str] = []
+#cria um usuario
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
-#classe para o ESTOQUE
-class Estoque(BaseModel):
-    quantidade: int
-    itens: list[Item]
+#obtem todos os usuarios cadastrados
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
 
-#classe para Mensagem
-class Message(BaseModel):
-    info : str
+#obtem um usuario em especifico
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-class Tags(Enum):
-    items:str = "Items"
-    inventario:str = "Inventário"
-    
-#decoretor para exception handler
-@app.exception_handler(StarletteHTTPException)
-async def custom_http_exception_handler(request, exc):
-    return await http_exception_handler(request, exc)
+#deleta um usuario
+@app.delete("/users/{user_id}", response_model=schemas.Message)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.delete_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
+#atualiza um usuario
+@app.put("/users/{user_id}", response_model=schemas.User)
+def update_user(user: schemas.UserCreate, user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.update_user(db=db, user_id=user_id, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request, exc):
-    return await request_validation_exception_handler(request, exc)
+#cria um item relacionado a um usuario
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
 
-@app.get("/item", 
-        status_code=status.HTTP_200_OK, 
-        tags=[Tags.inventario],
-        summary="Obtem todos os itens",
-        response_model=Estoque
-)
-async def get_itens():
-    """
-    Retorna um dicionario com duas chaves, uma sendo a quantidade de itens diferentes cadastrados 
-    e outra uma lista com os itens.
+#atualiza um item
+@app.put("/users/{user_id}/items/{item_id}/", response_model=schemas.Item)
+def update_item(
+    user_id: int, item_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.update_user_item(db=db, item=item, user_id=user_id, item_id=item_id)
 
-    Formato:
-        {
-            "tamanho": int,
-            "itens"  : list
-        }
-    
-    Exemplo:
+#deleta um item de um usuario
+@app.delete("/items/{item_id}/", response_model=schemas.Message)
+def delete_item(item_id: int, db: Session = Depends(get_db)):
+    return crud.delete_user_item(db=db, item_id=item_id)
 
-        {
-            "tamanho": 3,
-            "estoque": [
-                {
-                    "nome": "Flor de lótus",
-                    "preco": 50.2
-                },
-                {
-                    "nome": "Chá preto",
-                    "descricao": "Chá preto importado da China",
-                    "preco": 62
-                },
-                {
-                    "nome": "Canela",
-                    "descricao": "Canela em pau,
-                    "preco": 20,
-                    "tags": ["Importado", "Especiarias", "Tempero", "Em natura"]
-                }
-            ]
-        }
-            
-    """
-    
-    return  {"quantidade": len(items), "itens": items}
-
-
-@app.get("/item/{item_id}", 
-        response_model=Item, 
-        status_code=status.HTTP_200_OK, 
-        tags=[Tags.items],
-        summary="Obtem um item específico",
-)
-async def get_item(item_id: int = Path(title="O id correspondente ao item obter", ge=0)):
-    """
-    Retorna um dicionario com apenas o item requisitado e o seu id relacionado.
-
-    Argumentos:
-    
-        item_id (int): id do item cadastrado
-
-    Exceções:
-    
-        HTTPException: O item relacionado ao id fornecido não existe
-
-    Retorno:
-    
-        Item: retorna um item com nome, preço, 
-        além da descrição e tags relacionadas, caso possua
-    """
-    if item_id >= len(items):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
-    
-    return items[item_id]
-
-
-@app.put("/item/{item_id}", 
-        tags=[Tags.items],
-        response_model=Item, 
-        status_code=status.HTTP_200_OK, 
-        summary="Atualiza um item",
-)
-async def update_item(Item: Item,
-    item_id: int = Path(title="O id correspondente ao item que deseja atualizar", ge=0)
-    ):
-    """
-    Atualiza as informações relacionadas a um item com o id desejado
-    
-    Args:
-    
-        item_id (int): id relacionado a um item da base de dados
-
-    Exceções:
-    
-        HTTPException: o id não corresponde a um item da base para ser atualizado
-
-    Retorno:
-    
-        Item: o item que foi atualizado
-    """
-    
-    if item_id >= len(items):
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
-    
-    items[item_id] = Item
-    return Item 
-
-
-@app.delete("/item/{item_id}", 
-        status_code=status.HTTP_200_OK, 
-        tags=[Tags.items],
-        summary="Deleta um item",
-        response_model=Message,
-)
-async def delete_item(item_id: int = Path(title="O id correspondente ao item que deseja deletar", ge=0)):
-    """
-    Deleta um item da base de dados que corresponda ao id desejado
-    
-    Args:
-    
-        item_id (int):  id relacionado a um item da base de dados
-
-    Exceções:
-    
-        HTTPException: o id não corresponde a um item da base para ser deletado
-        
-    Retorno:
-    
-        info (str): Uma string informando que houve sucesso no 
-        processo de delete do item desejado
-    """
-    if item_id >= len(items):
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
-
-    del items[item_id]
-    return {"info": "o item foi deletado com sucesso"}
-    
-
-
-@app.post("/item", 
-        status_code=status.HTTP_201_CREATED, 
-        tags=[Tags.items],
-        summary="Cria um item",
-        response_model=Item, 
-)
-async def create_item(Item: Item):
-    """
-    Cria um item com todas as informações:
-
-    Args:
-    
-        Item (Item): Um item com informações como **Nome**, **Preco**, 
-        **Descricao** e uma lista de **Tags**
-
-    Retorno:
-    
-        Item: Retorna o item que foi adicionado no banco de dados
-    """
-    items.append(Item)
-
-    return Item
-    
+#obtem todos os items
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
